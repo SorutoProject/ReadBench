6bc0d3ec4547cee291b0f89f393bd54bd45b9495
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2020-10-22 16:50:35 +0900

    Update some features

--- components/readbench/readbench.js
+++ components/readbench/readbench.js
@@ -6,112 +6,128 @@
 */
 
 const readBench = {
-    //save the sentences array to use globally
-    sentences:[],
-    forceStopFlag: false,
-    loadPassage: function (passage) {
-        //Initialize #text
-        document.getElementById("text").innerHTML = "";
+  //save the sentences array to use globally
+  sentences: [],
+  forceStopFlag: false,
+  loadPassage: function(passage) {
+    //Initialize #text
+    document.getElementById("text").innerHTML = "";
 
-        //cut by sentences
-        //save to readBench.sentences
-        readBench.sentences = tokenizer.sentences(passage);
-        console.log(readBench.sentences);
+    //cut by sentences
+    //save to readBench.sentences
+    readBench.sentences = tokenizer.sentences(passage);
+    console.log(readBench.sentences);
 
-        //make <span>s into #text
-        for(let i = 0 ; i < readBench.sentences.length; i++){
-            const sentence = readBench.sentences[i];
-            const span = document.createElement("span");
-            span.innerText = sentence;
-            //add event to read from the sentence
-            span.addEventListener("click", function(){
-                readBench.speak(i, false);
-            });
-            document.getElementById("text").appendChild(span);
+    //make <span>s into #text
+    for (let i = 0; i < readBench.sentences.length; i++) {
+      let sentence = readBench.sentences[i];
+
+      const span = document.createElement("span");
+      span.innerText = sentence;
+      //add event to read from the sentence
+      span.addEventListener("click", function() {
+        readBench.speak(i, false);
+      });
+      span.addEventListener("contextmenu", function(){
+        if(confirm("選択された文から、この文章の最後までを読み上げますか？")){
+          readBench.speak(i,true);
         }
-    },
-    speak: function (readingSentenceNo = 0, goNext = true) {
+      });
 
-        const maxSentenceNo = readBench.sentences.length - 1;
-        let uttr = new SpeechSynthesisUtterance();
-        uttr.lang = "en-US";
-        uttr.speed = 2;
-        let speakSentence = function () {
-            const readingSentenceElem = document.querySelectorAll("div#text span")[readingSentenceNo];
-            uttr.text = readingSentenceElem.textContent;
-            document.querySelectorAll("div#text span").forEach(function (span) {
-                span.classList.remove("active");
-            });
-
-            readingSentenceElem.classList.add("active");
-
-            speechSynthesis.speak(uttr);
-        }
-        let next = function () {
-            //console.log([readingSentenceNo, maxSentenceNo]);
-            if (readingSentenceNo < maxSentenceNo && goNext === true) {
-                readingSentenceNo++;
-                speakSentence();
-            } else {
-                readBench.resetHighlight();
-            }
-        }
-        uttr.onend = function (e) {
-            if(readBench.forceStopFlag === false){
-                next();
-            }else{
-              readBench.forceStopFlag = false;
-              readBench.resetHighlight();
-            }
-        }
-
-        //start speaking
-        speakSentence();
-
-    },
-    stop:function(){
-        readBench.forceStopFlag = true;
-    },
-    resetHighlight: function(){
-        document.querySelectorAll("div#text span").forEach(function (span) {
-            span.classList.remove("active");
-        });
+      document.getElementById("text").appendChild(span);
     }
+  },
+  speak: function(readingSentenceNo = 0, goNext = true) {
+    const startSpeak = function() {
+      const maxSentenceNo = readBench.sentences.length - 1;
+      let uttr = new SpeechSynthesisUtterance();
+      uttr.lang = "en-US";
+      uttr.rate = 1.0;
+      let speakSentence = function() {
+        const readingSentenceElem = document.querySelectorAll("div#text span")[readingSentenceNo];
+        uttr.text = readingSentenceElem.textContent;
+        document.querySelectorAll("div#text span").forEach(function(span) {
+          span.classList.remove("active");
+        });
+
+        readingSentenceElem.classList.add("active");
+
+        speechSynthesis.speak(uttr);
+      }
+      let next = function() {
+        //console.log([readingSentenceNo, maxSentenceNo]);
+        if (readingSentenceNo < maxSentenceNo && goNext === true) {
+          readingSentenceNo++;
+          speakSentence();
+        } else {
+          readBench.resetHighlight();
+        }
+      }
+      uttr.onend = function(e) {
+        if (readBench.forceStopFlag === false) {
+          next();
+        } else {
+          readBench.forceStopFlag = false;
+          readBench.resetHighlight();
+        }
+      }
+
+      //start speaking
+      speakSentence();
+    }
+    if (speechSynthesis.speaking === true) {
+      readBench.stop();
+      setTimeout(function(){
+        startSpeak();
+      },250);
+    } else {
+      startSpeak();
+    }
+
+
+  },
+  stop: function() {
+    readBench.forceStopFlag = true;
+    speechSynthesis.cancel();
+  },
+  resetHighlight: function() {
+    document.querySelectorAll("div#text span").forEach(function(span) {
+      span.classList.remove("active");
+    });
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("file-button").addEventListener("click", function(){
-        //make an input[type="file"] element
-        const fileElem = document.createElement("input");
-        fileElem.type = "file";
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("file-button").addEventListener("click", function() {
+    //make an input[type="file"] element
+    const fileElem = document.createElement("input");
+    fileElem.type = "file";
 
-        //read only .txt
-        fileElem.accept = ".txt";
+    //read only .txt
+    fileElem.accept = ".txt";
 
-        //add Event to fileElem
-        fileElem.addEventListener("change", function(e){
-            const reader = new FileReader();
-            reader.onload = function(){
-                readBench.loadPassage(reader.result);
-            }
-            reader.readAsText(e.target.files[0]);
-        });
-
-        //open the dialog
-        fileElem.click();
-    });
-
-    document.getElementById("speak-button").addEventListener("click", function(){
-        readBench.speak(0,true);
-    });
-
-    document.getElementById("stop-button").addEventListener("click", function(){
+    //add Event to fileElem
+    fileElem.addEventListener("change", function(e) {
+      const reader = new FileReader();
+      reader.onload = function() {
+        readBench.loadPassage(reader.result);
         readBench.stop();
-        M.toast({
-          html:`現在読み上げ中のテキストが読み終わると停止します。`
-        })
+      }
+      reader.readAsText(e.target.files[0]);
     });
 
-    //load sample text
-    readBench.loadPassage(`Please click the upload button and select a txt file to read.`);
+    //open the dialog
+    fileElem.click();
+  });
+
+  document.getElementById("speak-button").addEventListener("click", function() {
+    readBench.speak(0, true);
+  });
+
+  document.getElementById("stop-button").addEventListener("click", function() {
+    readBench.stop();
+  });
+
+  //load sample text
+  readBench.loadPassage(`Please click the upload button and select a txt file to read.`);
 });
